8c2de641c7e4149da9de804cf391fbb7c5dc424f
khai4299 <tuankhai040299@gmail.com>
2023-05-06 21:47:04 +0700

    v1.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,17 @@ import EmptyState from "@/app/components/EmptyState";
 import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
-
 interface HomeProps {
 	searchParams: IListingsParams;
 }
 
 const Home = async ({ searchParams }: HomeProps) => {
-	const listings = await getListings(searchParams);
 	const currentUser = await getCurrentUser();
 
+	const listings = await getListings({
+		...searchParams,
+		userId: currentUser?.id,
+	});
 	if (listings.length === 0) {
 		return (
 			<ClientOnly>
